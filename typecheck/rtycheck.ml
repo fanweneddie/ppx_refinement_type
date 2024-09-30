7d003c8f696c25508f7053a74a034ecc772df7a1
ruiz-m <ruizmc@umich.edu>
2024-09-30 10:22:40 -0400

    Begin implementation of type synthesis

--- typecheck/rtycheck.ml
+++ typecheck/rtycheck.ml
@@ -1,4 +1,13 @@
 open Ocaml_common
 open Rty
 
-let bidirect_type_infer (_ctx: rty_ctx) (_struc: Typedtree.structure): rty option = None
+let bidirect_type_infer (_ctx: rty_ctx) (_struc: Typedtree.structure_item) 
+  (ty: rty option) : rty option =
+  match ty with
+  | None -> failwith "NI"(* Type synthesis *)
+  | Some _ty' -> failwith "NI" (* Type unify with ty and ty' and then do type check *) 
+
+let type_infer (_ctx: rty_ctx) (struc: Typedtree.structure_item) : rty option =
+  match struc.str_desc with
+  | Tstr_value (_, _vb) -> failwith "NI" (* Regular type checking *)
+  | _ -> failwith "NI" (* Figure out the rest*)
